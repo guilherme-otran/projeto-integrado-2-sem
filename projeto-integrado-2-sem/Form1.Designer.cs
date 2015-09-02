0e0e4597509d6a09e462c991673ddafca2e9afec
Valmir Massoni Jr. <valmirmassoni@hotmail.com>
2015-09-02 20:48:35 -0300

    Tela de login

--- projeto-integrado-2-sem/Form1.Designer.cs
+++ projeto-integrado-2-sem/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsi_about = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsi_close = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(192, 164);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(168, 20);
+            this.txt_pass.TabIndex = 0;
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(192, 102);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(168, 20);
+            this.txt_user.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(234, 199);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsi_about,
+            this.tsi_close});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsi_about
+            // 
+            this.tsi_about.Name = "tsi_about";
+            this.tsi_about.Size = new System.Drawing.Size(49, 20);
+            this.tsi_about.Text = "Sobre";
+            // 
+            // tsi_close
+            // 
+            this.tsi_close.Name = "tsi_close";
+            this.tsi_close.Size = new System.Drawing.Size(38, 20);
+            this.tsi_close.Text = "Sair";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(544, 333);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Projeto Integrado";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsi_about;
+        private System.Windows.Forms.ToolStripMenuItem tsi_close;
     }
 }
 
