6fa560f85fe2e3861145e07d2c6e5e3ad627d818
guilherme <guilherme_?otran@hotmail.com>
2015-09-02 22:23:49 -0300

    Classes Base

---
+++ projeto-integrado-2-sem/IStorable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_integrado_2_sem
+{
+    interface IStorable<K>
+    {
+        public string[] AsStringArray();
+        public static K FromStringArray(string[] data);
+    }
+}
---
+++ projeto-integrado-2-sem/user.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_integrado_2_sem
+{
+    class User : IStorable<User>
+    {
+        public string id;
+        public string username;
+        public string password;
+
+        public User()
+        {
+            this.id = null;
+            this.username = null;
+            this.password = null;
+        }
+
+        public User(string id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+
+        public string[] AsStringArray()
+        {
+            return new string[] { id, username, password };
+        }
+
+        public static User FromStringArray(string[] data)
+        {
+            return new User(data[0], data[1], data[2]);
+        }
+
+    }
+}
