0a86b5d7498f2e73f60818f94a7c6bcb3d0b223f
Valmir Massoni Jr <valmirmassoni@hotmail.com>
2015-09-18 22:40:34 -0300

    Continuação da calculadora, adaptação do form conforme os radio buttons são selecionados, termino da função A e B e inicio da funcionalidade C

--- projeto-integrado-2-sem/FormDate.Designer.cs
+++ projeto-integrado-2-sem/FormDate.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.rdbAge = new System.Windows.Forms.RadioButton();
             this.rdbInterval = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rdbAD = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.gboInput = new System.Windows.Forms.GroupBox();
@@ -38,15 +38,22 @@
             this.txtDay = new System.Windows.Forms.TextBox();
             this.cmbYear = new System.Windows.Forms.ComboBox();
             this.cmbMonth = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblInput1 = new System.Windows.Forms.Label();
             this.lblOutput = new System.Windows.Forms.Label();
+            this.txtDay2 = new System.Windows.Forms.TextBox();
+            this.cmbYear2 = new System.Windows.Forms.ComboBox();
+            this.cmbMonth2 = new System.Windows.Forms.ComboBox();
+            this.lblInput2 = new System.Windows.Forms.Label();
+            this.rdbAdd = new System.Windows.Forms.RadioButton();
+            this.rdbDec = new System.Windows.Forms.RadioButton();
             this.gboInput.SuspendLayout();
             this.SuspendLayout();
             // 
             // rdbAge
             // 
             this.rdbAge.AutoSize = true;
-            this.rdbAge.Location = new System.Drawing.Point(46, 66);
+            this.rdbAge.Checked = true;
+            this.rdbAge.Location = new System.Drawing.Point(74, 66);
             this.rdbAge.Name = "rdbAge";
             this.rdbAge.Size = new System.Drawing.Size(52, 17);
             this.rdbAge.TabIndex = 4;
@@ -58,33 +65,32 @@
             // rdbInterval
             // 
             this.rdbInterval.AutoSize = true;
-            this.rdbInterval.Location = new System.Drawing.Point(114, 66);
+            this.rdbInterval.Location = new System.Drawing.Point(142, 66);
             this.rdbInterval.Name = "rdbInterval";
             this.rdbInterval.Size = new System.Drawing.Size(110, 17);
             this.rdbInterval.TabIndex = 5;
-            this.rdbInterval.TabStop = true;
             this.rdbInterval.Text = "Intervalo de datas";
             this.rdbInterval.UseVisualStyleBackColor = true;
+            this.rdbInterval.CheckedChanged += new System.EventHandler(this.rdbInterval_CheckedChanged);
             // 
-            // radioButton1
+            // rdbAD
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(235, 66);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(188, 17);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Acrescimo/Decrescimo de periodo";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rdbAD.AutoSize = true;
+            this.rdbAD.Location = new System.Drawing.Point(263, 66);
+            this.rdbAD.Name = "rdbAD";
+            this.rdbAD.Size = new System.Drawing.Size(188, 17);
+            this.rdbAD.TabIndex = 6;
+            this.rdbAD.Text = "Acrescimo/Decréscimo de periodo";
+            this.rdbAD.UseVisualStyleBackColor = true;
+            this.rdbAD.CheckedChanged += new System.EventHandler(this.rdbAD_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(429, 66);
+            this.radioButton2.Location = new System.Drawing.Point(457, 66);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(100, 17);
             this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Análise de Data";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
@@ -92,7 +98,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(44, 27);
+            this.label1.Location = new System.Drawing.Point(72, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(332, 20);
             this.label1.TabIndex = 4;
@@ -100,14 +106,18 @@
             // 
             // gboInput
             // 
+            this.gboInput.Controls.Add(this.rdbDec);
+            this.gboInput.Controls.Add(this.rdbAdd);
+            this.gboInput.Controls.Add(this.lblInput2);
             this.gboInput.Controls.Add(this.btnProcess);
             this.gboInput.Controls.Add(this.txtDay);
             this.gboInput.Controls.Add(this.cmbYear);
+            this.gboInput.Controls.Add(this.cmbMonth2);
             this.gboInput.Controls.Add(this.cmbMonth);
-            this.gboInput.Controls.Add(this.label2);
-            this.gboInput.Location = new System.Drawing.Point(48, 113);
+            this.gboInput.Controls.Add(this.lblInput1);
+            this.gboInput.Location = new System.Drawing.Point(76, 113);
             this.gboInput.Name = "gboInput";
-            this.gboInput.Size = new System.Drawing.Size(481, 108);
+            this.gboInput.Size = new System.Drawing.Size(480, 110);
             this.gboInput.TabIndex = 8;
             this.gboInput.TabStop = false;
             // 
@@ -128,7 +138,7 @@
             this.txtDay.Name = "txtDay";
             this.txtDay.Size = new System.Drawing.Size(57, 20);
             this.txtDay.TabIndex = 0;
-            this.txtDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            this.txtDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dates_KeyPress);
             // 
             // cmbYear
             // 
@@ -136,58 +146,141 @@
             this.cmbYear.Items.AddRange(new object[] {
             "{ 1900 , ..., 2015}"});
             this.cmbYear.Location = new System.Drawing.Point(239, 59);
+            this.cmbYear.MaxLength = 4;
             this.cmbYear.Name = "cmbYear";
             this.cmbYear.Size = new System.Drawing.Size(66, 21);
             this.cmbYear.TabIndex = 2;
+            this.cmbYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dates_KeyPress);
             // 
             // cmbMonth
             // 
+            this.cmbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbMonth.FormattingEnabled = true;
             this.cmbMonth.Items.AddRange(new object[] {
-            "01 - Janeiro",
-            "02 - Fevereiro",
-            "03 - Março",
-            "04 - Abril",
-            "05 - Maio",
-            "06 - Junho",
-            "07 - Julho",
-            "08 - Agosto",
-            "09 - Setembro",
-            "10 - Outubro",
-            "11 - Novembro",
-            "12 - Dezembro"});
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"});
             this.cmbMonth.Location = new System.Drawing.Point(103, 59);
             this.cmbMonth.Name = "cmbMonth";
             this.cmbMonth.Size = new System.Drawing.Size(121, 21);
             this.cmbMonth.TabIndex = 1;
             // 
-            // label2
+            // lblInput1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 26);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Insira sua data de nascimento:";
+            this.lblInput1.AutoSize = true;
+            this.lblInput1.Location = new System.Drawing.Point(15, 28);
+            this.lblInput1.Name = "lblInput1";
+            this.lblInput1.Size = new System.Drawing.Size(0, 13);
+            this.lblInput1.TabIndex = 0;
             // 
             // lblOutput
             // 
             this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(287, 253);
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(252, 245);
             this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.Size = new System.Drawing.Size(0, 20);
             this.lblOutput.TabIndex = 6;
+            // 
+            // txtDay2
+            // 
+            this.txtDay2.Location = new System.Drawing.Point(107, 223);
+            this.txtDay2.MaxLength = 2;
+            this.txtDay2.Name = "txtDay2";
+            this.txtDay2.Size = new System.Drawing.Size(57, 20);
+            this.txtDay2.TabIndex = 4;
+            this.txtDay2.Visible = false;
+            this.txtDay2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dates_KeyPress);
+            // 
+            // cmbYear2
+            // 
+            this.cmbYear2.FormattingEnabled = true;
+            this.cmbYear2.Items.AddRange(new object[] {
+            "{ 1900 , ..., 2015}"});
+            this.cmbYear2.Location = new System.Drawing.Point(315, 222);
+            this.cmbYear2.MaxLength = 4;
+            this.cmbYear2.Name = "cmbYear2";
+            this.cmbYear2.Size = new System.Drawing.Size(66, 21);
+            this.cmbYear2.TabIndex = 6;
+            this.cmbYear2.Visible = false;
+            this.cmbYear2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dates_KeyPress);
+            // 
+            // cmbMonth2
+            // 
+            this.cmbMonth2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMonth2.FormattingEnabled = true;
+            this.cmbMonth2.Items.AddRange(new object[] {
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"});
+            this.cmbMonth2.Location = new System.Drawing.Point(103, 109);
+            this.cmbMonth2.Name = "cmbMonth2";
+            this.cmbMonth2.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonth2.TabIndex = 5;
+            this.cmbMonth2.Visible = false;
+            // 
+            // lblInput2
+            // 
+            this.lblInput2.AutoSize = true;
+            this.lblInput2.Location = new System.Drawing.Point(15, 91);
+            this.lblInput2.Name = "lblInput2";
+            this.lblInput2.Size = new System.Drawing.Size(0, 13);
+            this.lblInput2.TabIndex = 4;
+            // 
+            // rdbAdd
+            // 
+            this.rdbAdd.AutoSize = true;
+            this.rdbAdd.Checked = true;
+            this.rdbAdd.Location = new System.Drawing.Point(349, 72);
+            this.rdbAdd.Name = "rdbAdd";
+            this.rdbAdd.Size = new System.Drawing.Size(82, 17);
+            this.rdbAdd.TabIndex = 6;
+            this.rdbAdd.TabStop = true;
+            this.rdbAdd.Text = "Acrescentar";
+            this.rdbAdd.UseVisualStyleBackColor = true;
+            this.rdbAdd.Visible = false;
+            // 
+            // rdbDec
+            // 
+            this.rdbDec.AutoSize = true;
+            this.rdbDec.Location = new System.Drawing.Point(349, 95);
+            this.rdbDec.Name = "rdbDec";
+            this.rdbDec.Size = new System.Drawing.Size(62, 17);
+            this.rdbDec.TabIndex = 7;
+            this.rdbDec.Text = "Diminuir";
+            this.rdbDec.UseVisualStyleBackColor = true;
+            this.rdbDec.Visible = false;
             // 
             // FormDate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 398);
+            this.ClientSize = new System.Drawing.Size(629, 446);
+            this.Controls.Add(this.txtDay2);
             this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.cmbYear2);
             this.Controls.Add(this.gboInput);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rdbAD);
             this.Controls.Add(this.rdbInterval);
             this.Controls.Add(this.rdbAge);
             this.Name = "FormDate";
@@ -204,15 +297,21 @@
 
         private System.Windows.Forms.RadioButton rdbAge;
         private System.Windows.Forms.RadioButton rdbInterval;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rdbAD;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox gboInput;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInput1;
         private System.Windows.Forms.TextBox txtDay;
         private System.Windows.Forms.ComboBox cmbYear;
         private System.Windows.Forms.ComboBox cmbMonth;
         private System.Windows.Forms.Button btnProcess;
         private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtDay2;
+        private System.Windows.Forms.ComboBox cmbYear2;
+        private System.Windows.Forms.ComboBox cmbMonth2;
+        private System.Windows.Forms.Label lblInput2;
+        private System.Windows.Forms.RadioButton rdbDec;
+        private System.Windows.Forms.RadioButton rdbAdd;
     }
 }
--- projeto-integrado-2-sem/FormDate.cs
+++ projeto-integrado-2-sem/FormDate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projeto_integrado_2_sem.Interactors;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,11 +9,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace projeto_integrado_2_sem
 {
     public partial class FormDate : Form
     {
-        private bool validateDate(int day, int month, int year)
+
+        public FormDate()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDate_Load(object sender, EventArgs e)
+        {
+            cmbYear.DataSource = Enumerable.Range(1950, ((DateTime.Today.Year)-1949)).ToList();
+            cmbYear2.DataSource = Enumerable.Range(1950, ((DateTime.Today.Year) - 1949)).ToList();
+        }
+
+        private bool validateDate(int day, int month, int year) // Função para validar a data
         {
             int lastDay = DateTime.DaysInMonth(year, month); // obtem o último dia do mês, ou seja, quantos dias têm o mês.
 
@@ -21,26 +35,59 @@ namespace projeto_integrado_2_sem
             else
                 return false;
         }
-        public FormDate()
-        {
-            InitializeComponent();
-        }
-
-        private void FormDate_Load(object sender, EventArgs e)
-        {
-            rdbAge.Checked = true;
-            cmbYear.DataSource = Enumerable.Range(1900, ((DateTime.Today.Year)-1900)).ToList();
-        }
 
         private void rdbAge_CheckedChanged(object sender, EventArgs e)
         {
             if (rdbAge.Checked)
             {
-                gboInput.Text = "Calcular Idade";
+                gboInput.Size = new Size(480, 110);
+                lblOutput.Location = new Point(224, 245);
+                gboInput.Text = "Calcular idade";
+                lblInput1.Text = "Digite sua data de nascimento:";
+                btnProcess.Location = new Point(335, 57);
             }
         }
 
-        private void txtDay_KeyPress(object sender, KeyPressEventArgs e)
+        private void rdbInterval_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdbInterval.Checked)
+            {
+                gboInput.Size = new Size(480, 180);
+                btnProcess.Location = new Point(336, 80);
+                lblOutput.Location = new Point(224, 295);
+                gboInput.Text = "Calcular o intervalo entre duas datas";
+                lblInput1.Text = "Digite a primeira data: ";
+                lblInput2.Text = "Digite a segunda data: ";
+                txtDay2.Visible = true;
+                cmbMonth2.Visible = true;
+                cmbYear2.Visible = true;
+            }
+            else
+            {
+                lblInput2.Text = "";
+                txtDay2.Visible = false;
+                cmbMonth2.Visible = false;
+                cmbYear2.Visible = false;
+
+            }
+        }
+
+        private void rdbAD_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdbAD.Checked)
+            {
+                btnProcess.Location = new Point(203, 150);
+                rdbAdd.Visible = true;
+                rdbDec.Visible = true;
+            }
+            else
+            {
+                rdbAdd.Visible = false;
+                rdbDec.Visible = false;
+            }
+        }
+
+        private void dates_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!(char.IsNumber(e.KeyChar)))
                 e.KeyChar = char.MinValue;
@@ -48,17 +95,73 @@ namespace projeto_integrado_2_sem
 
         private void btnProcess_Click(object sender, EventArgs e)
         {
-            if (validateDate(int.Parse(txtDay.Text), cmbMonth.SelectedIndex, int.Parse(cmbYear.Text)))
-            { 
-                /*DateTime birthDate = new DateTime();
-                 *                
-                 *TimeSpan age = DateCalculator.ageCalc(birthDate);                   TimeSpan FAIL!!
-                 *                                                                       C# huehue br
-                 *lblOutput.Text = "Sua Idade é " + ;                   ERROR 404      TimeSpan.Year not found. 
-                 *                                                                          
-                 */
+            if (rdbAge.Checked)
+            {
+                lblOutput.Text = "";
+                try
+                {
+                    if (validateDate(int.Parse(txtDay.Text), cmbMonth.SelectedIndex + 1, int.Parse(cmbYear.Text)))
+                    {
+                        DateTime birthDate = new DateTime(int.Parse(cmbYear.Text), cmbMonth.SelectedIndex + 1, int.Parse(txtDay.Text));
+
+                        MessageBox.Show("data " + birthDate);
+
+                        TimeSpan age = DateCalculator.ageCalc(birthDate);
+
+                        lblOutput.Text = "Sua Idade é:\n";
+                        lblOutput.Text += "\nEm anos: " + ((int)((age.Days) / 365.25));
+                        lblOutput.Text += "\nEm meses: " + ((int)(((age.Days) / 365.25) * 12));
+                        lblOutput.Text += "\nEm dias: " + age.Days;
+                        lblOutput.Text += "\nEm horas: " + age.TotalHours;
+                    }
+                    else
+                    {
+                        txtDay.Text = "";
+                        MessageBox.Show("Data inválida");
+                    }
+                }
+                catch
+                {
+                    txtDay.Text = "";
+                    MessageBox.Show("Preencha todos os campos com valores corretos!");
+                }
+
             }
 
+            if(rdbInterval.Checked)
+            {
+                lblOutput.Text = "";
+                try
+                {
+                    if (validateDate(int.Parse(txtDay.Text), cmbMonth.SelectedIndex + 1, int.Parse(cmbYear.Text)) && validateDate(int.Parse(txtDay2.Text), cmbMonth2.SelectedIndex + 1, int.Parse(cmbYear2.Text)))
+                    {
+                        DateTime date1 = new DateTime(int.Parse(cmbYear.Text), cmbMonth.SelectedIndex + 1, int.Parse(txtDay.Text));
+                        DateTime date2 = new DateTime(int.Parse(cmbYear2.Text), cmbMonth2.SelectedIndex + 1, int.Parse(txtDay2.Text));
+
+                        TimeSpan interval = DateCalculator.timeInterval(date1, date2);
+
+                        lblOutput.Text = "O intervalo é:\n";
+                        lblOutput.Text += "\nEm anos: " + ((int)((interval.Days) / 365.25));
+                        lblOutput.Text += "\nEm meses: " + ((int)(((interval.Days) / 365.25) * 12));
+                        lblOutput.Text += "\nEm dias: " + interval.Days;
+                        lblOutput.Text += "\nEm horas: " + interval.TotalHours;
+                    }
+                    else
+                    {
+                        txtDay.Text = "";
+                        txtDay2.Text = "";
+                        MessageBox.Show("Data inválida");
+                    }
+                }
+                catch
+                {
+                    txtDay.Text = "";
+                    MessageBox.Show("Preencha todos os campos com valores corretos!");
+                }
+            }
+            if(rdbAD.Checked)
+            {
+            }
         }
     }
 }
