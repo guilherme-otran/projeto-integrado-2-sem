177f826f78c2ca1e63c83e75ce58de45f9cd118e
Valmir Massoni Jr <valmirmassoni@hotmail.com>
2015-09-25 11:31:58 -0300

    Testes automatizados da classe DateCalculator finalizados

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,12 +9,56 @@ namespace UnitTestProject1
     public class DateCalculatorTest
     {
         [TestMethod]
-        public void Testbissextile_when_is_bissextile()
+        public void TestageCalc()
+        {
+            TimeSpan test = DateCalculator.ageCalc(new DateTime(1997, 9, 17));
+            Assert.AreEqual(TimeSpan.Parse("6582:00:00:00"), test); //Testado dia 25/09/2015, para datas posteriores, calcular e mudar o parâmetro a ser enviado
+        }
+        [TestMethod]
+        public void TestageCalc_When_birthdate_is_greater_than_current_date()
+        {
+            bool test;
+            try 
+            { 
+                DateCalculator.ageCalc(new DateTime(2016, 1, 1));
+                test = false;
+            }
+            catch (DateCalculator.InvalidBornDate) 
+            { 
+                test = true;
+                Assert.AreEqual(true, test); 
+            }
+        }
+        [TestMethod]
+        public void TesttimeInterval()
+        {
+            TimeSpan test = DateCalculator.timeInterval(new DateTime(2014,01,01), new DateTime(2015,01,01));
+            Assert.AreEqual(TimeSpan.Parse("365:00:00:00"),test);
+        }
+        [TestMethod]
+        public void TestdateIncrease()
+        {
+            DateTime test = DateCalculator.dateIncrease(new DateTime(2014,01,01), TimeSpan.Parse("365:00:00:00"));
+            Assert.AreEqual(new DateTime(2015, 01, 01), test);
+        }
+        [TestMethod]
+        public void TestdateDecrease()
+        {
+            DateTime test = DateCalculator.dateDecrease(new DateTime(2014, 01, 01), TimeSpan.Parse("365:00:00:00"));
+            Assert.AreEqual(new DateTime(2013, 01, 01), test);
+        }
+        [TestMethod]
+        public void Testbissextile_when_is_bissextile_divisible_by_4_but_not_by_100()
         {
             bool test = DateCalculator.bissextile(2016);
             Assert.AreEqual(true, test);
         }
-
+        [TestMethod]
+        public void Testbissextile_when_is_bissextile_divisible_by_400()
+        {
+            bool test = DateCalculator.bissextile(2000);
+            Assert.AreEqual(true, test);
+        }
         [TestMethod]
         public void Testbissextile_when_is_not_bissextile()
         {
