01045d33f08bdea7f09e7c9a6acfddb2f338a09b
Guilherme <guilherme_otran@hotmail.com>
2015-10-22 19:20:37 -0200

    NUnit

--- UnitTestProject1/PasswordValidatorUnitTest.cs
+++ UnitTestProject1/PasswordValidatorUnitTest.cs
@@ -9,7 +9,7 @@ using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 #endif
 
-// using NUnitAssert = NUnit.Framework.Assert;
+using NUnitAssert = NUnit.Framework.Assert;
 using MsAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using projeto_integrado_2_sem.Validators;
 using projeto_integrado_2_sem.Models;
