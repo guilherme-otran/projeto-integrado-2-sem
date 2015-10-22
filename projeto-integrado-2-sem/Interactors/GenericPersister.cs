450a2a397cc1ce3da4645c9a89c6f51bdeef110d
Guilherme Otrant <gui@hotmail.com>
2015-10-21 22:18:52 -0200

    Crazy things

---
+++ projeto-integrado-2-sem/Interactors/GenericPersister.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projeto_integrado_2_sem.Repositories;
+using projeto_integrado_2_sem.Validators;
+
+namespace projeto_integrado_2_sem.Interactors
+{
+    class GenericPersister<T>
+    {
+        private BaseRepository<T> repository;
+        private Validator<T>[] validators;
+
+        public GenericPersister(BaseRepository<T> repo, Validator<T>[] validators)
+        {
+            this.repository = repo;
+            this.validators = validators;
+        }
+
+        public bool persist(T record)
+        {
+            var results = validators.Select(v => v.Validate(record));
+            if (results.All(r => r.Valid()))
+            {
+                repository.persist(record);
+            } 
+            
+            return false;
+        }
+    }
+}
