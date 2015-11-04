84a24f3251ff2bf1542cd0c080c6651f7f448497
guilherme <guilherme_otran@hotmail.com>
2015-11-04 19:59:12 -0200

    Gravando todos os dados do user

--- projeto-integrado-2-sem/StorableAdapters/UserStorableAdapter.cs
+++ projeto-integrado-2-sem/StorableAdapters/UserStorableAdapter.cs
@@ -10,11 +10,11 @@ namespace projeto_integrado_2_sem
 {
     class UserStorableAdapter : IStorableAdapter<User>
     {
-        public const int EMAIL_INDEX = 0;
+        public const int EMAIL_INDEX = 2;
 
         public int AttributeCount()
         {
-            return 3;
+            return 9;
         }
 
         public string[] AsStringArray(User storable)
@@ -24,15 +24,41 @@ namespace projeto_integrado_2_sem
             if (storable.Profile != null)
                 profileId = storable.Profile.id;
 
-            return new string[] { storable.email, storable.password, profileId };
+            return new string[] { 
+                storable.code, 
+                storable.name, 
+                storable.email,
+                storable.password, 
+                storable.oldPassword, 
+                storable.passwordChangeDate.ToBinary().ToString(), 
+                storable.birthDate.ToBinary().ToString(), 
+                storable.status.ToString(), 
+                profileId 
+            };
         }
 
         public User FromStringArray(string identifier, string[] data)
         {
-            if (data[2] == Profile.AdminProfile().id)
-                return new User(identifier, data[0], data[1], Profile.AdminProfile());
+            Profile prof = null;
 
-            return new User(identifier, data[0], data[1], Profile.Operator());
+            if (data[8] == Profile.AdminProfile().id)
+                prof = Profile.AdminProfile();
+            if (data[8] == Profile.Assistant().id)
+                prof = Profile.Assistant();
+            if (prof == null)
+                prof = Profile.Operator();
+
+            var user = new User();
+            user.code = data[0];
+            user.name = data[1];
+            user.email = data[2];
+            user.password = data[3];
+            user.oldPassword = data[4];
+            user.passwordChangeDate = DateTime.FromBinary(long.Parse(data[5]));
+            user.birthDate = DateTime.FromBinary(long.Parse(data[6]));
+            user.status = int.Parse(data[7]);
+            user.Profile = prof;
+            return user;
         }
 
         public string Identifier(User storable)
