0c7d10db8397f53fc8dc7936f97860e3124f9d44
Guilherme <guilherme_otran@hotmail.com>
2015-11-27 21:59:22 -0200

    Model customer

---
+++ projeto-integrado-2-sem/Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_integrado_2_sem.Models
+{
+    [Serializable]
+    class Customer : IStorable, ICloneable
+    {
+        public string id;
+        protected string name;
+        protected string cpf;
+        protected Status currentStatus;
+
+        // View
+        public string Id { get { return id; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Cpf { get { return cpf; } set { cpf = value; } }
+        public Status CurrentStatus { get { return currentStatus; } set { currentStatus = value; } }
+
+        public enum Status
+        {
+            ACTIVE = 0,
+            INACTIVE
+        }
+
+        string IStorable.id
+        {
+            get { return this.Id; }
+            set { this.id = value; }
+        }
+
+        public object Clone()
+        {
+            var customer = new Customer();
+            customer.id = this.id;
+            customer.name = this.name;
+            customer.cpf = this.cpf;
+            customer.currentStatus = this.currentStatus;
+
+            return customer;
+        }
+    }
+}
