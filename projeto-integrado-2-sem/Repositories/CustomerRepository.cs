70497f1902de62de7beb788ee6ea2d7e69bf74a7
Guilherme <guilherme_otran@hotmail.com>
2015-11-27 22:05:21 -0200

    Repositorio de Customers

---
+++ projeto-integrado-2-sem/Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projeto_integrado_2_sem.Models;
+using System.IO;
+
+namespace projeto_integrado_2_sem.Repositories
+{
+    class CustomerRepository : BaseRepository<Customer>
+    {
+        public CustomerRepository() :
+            base(new BufferedStream(new FileStream("C:\\temp\\customers.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite)))
+        {
+        }
+
+        public override void InitializeFile()
+        {
+            base.InitializeFile(new CustomerStorableAdapter());
+        }
+    }
+
+}
---
+++ projeto-integrado-2-sem/StorableAdapters/CustomerStorableAdapter.cs
@@ -0,0 +1,38 @@
+﻿using projeto_integrado_2_sem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_integrado_2_sem
+{
+    [Serializable]
+    class CustomerStorableAdapter : IStorableAdapter<Customer>
+    {
+        private int autoIncrementValue = 1;
+
+        public int AttributeCount()
+        {
+            return 3;
+        }
+
+        public string Identifier(Customer storable)
+        {
+            return storable.Id;
+        }
+
+        public string DefineIdentifier(Customer storable)
+        {
+            storable.id = autoIncrementValue.ToString().PadLeft(6, '0');
+            autoIncrementValue++;
+            return Identifier(storable);
+        }
+
+        public Customer FromSerializedToPublic(Customer stored)
+        {
+            return (Customer)stored.Clone();
+        }
+    }
+}
